5159cb2a913d49edb938b0d9bced5afa0dad41a5
leonid.mashlyakevich <leonid.mashlyakevich@gmail.com>
2022-02-26 09:39:11 +0300

    Add project files.

---
+++ FirstTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Введите число от 0 до 100: ");
+            int value = int.Parse(Console.ReadLine());
+            
+            if (value >= 0 && value <= 30)
+            {
+                Console.WriteLine("Число находится в промежетке 0-30");
+            }
+            else if (value >= 31 && value <= 60)
+            {
+                Console.WriteLine("Число находится в промежетке 31-60");
+            }
+            else if (value >= 61 && value <= 100)
+            {
+                Console.WriteLine("Число находится в промежетке 60-100");
+            }
+            else
+            {
+                Console.WriteLine("Я такого числа не знаю");
+            }
+        }
+    }
+}
---
+++ SecondTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lowBorder = 200;
+            var higtBorder = 500;
+           
+            string result = string.Empty;
+            string resultString = string.Empty;
+            while (lowBorder<higtBorder)
+            {
+                if (lowBorder % 17 == 0)
+                { 
+                    result = $"{lowBorder},";
+                    resultString += result;                   
+                }
+              
+                lowBorder++;
+
+            }
+
+            Console.WriteLine(resultString);
+        }
+    }
+}
